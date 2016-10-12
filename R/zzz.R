14b381bf931953ebd22065e852a4b2992990ce8a
Keith Hughitt <keith.hughitt@gmail.com>
2016-10-12 16:16:00 -0400

    Updated package metadata; added manpage for package

--- R/zzz.R
+++ R/zzz.R
@@ -3,10 +3,9 @@
 
     packageStartupMessage(
         sprintf("\nThis is EuPathDB version %s\n Read 'EuPathDB()' to get started.\n", 
-                packageVersion("EuPathDB"))
+                packageVersion("EuPathDB")))
 
-    if (interactive() && .Platform$OS.type == "windows" &&
-        .Platform$GUI == "Rgui") {
+    if (interactive() && .Platform$OS.type == "windows" && .Platform$GUI == "Rgui") {
         addVigs2WinMenu("EuPathDB")
     }
 }
