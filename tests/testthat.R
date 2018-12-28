b32893038c344dd4634bd8f821b10e7db4aa43a5
Ashton Trey Belew <abelew@gmail.com>
2018-12-28 11:25:09 -0500

    oh my goodness, these capital letters are killing me.
    It is EuPathDB, not EuPathDb

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,10 @@
 #!/usr/bin/env Rscript
 library(methods)
 library(testthat)
-library(EuPathDb)
+library(EuPathDB)
 
 all <- data.frame()
-result <- 0 
+result <- 0
 
 message("Beginning test_dir('travis')")
 test_result <- try(testthat::test_dir("tests/travis", reporter="summary"))
@@ -12,4 +12,3 @@ if (class(test_result) == "try-error") {
   result <- result + 1
   test_result <- data.frame()
 }
-
