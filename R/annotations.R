d1024d72d1185566eb38f7871e571c2d8b8082b6
Ashton Trey Belew <abelew@gmail.com>
2019-02-01 15:12:11 -0500

    I accidently messed up the ortholog generator.
    I have a comment asking why I made a specific query.  Here is the
    comment:
    "I am not sure what I was doing with the following 5 lines of code."
    
    Well, those lines were to get the set of species for which to collect
    the orthologs.  Unfortunately, I immediately thereafter did not use that
    set of species and so did not actually download the orthologs.
    
    I fixed this and will remove the comment now.

--- R/annotations.R
+++ R/annotations.R
@@ -536,6 +536,45 @@ get_eupath_pkgnames <- function(species="Coprinosis.cinerea.okayama7#130", versi
   return(pkg_list)
 }
 
+#' Shortcut for loading annotation data from a eupathdb-based orgdb.
+#'
+#' Every time I go to load the annotation data from an orgdb for a parasite, it
+#' takes me an annoyingly long time to get the darn flags right.  As a result I
+#' wrote this to shortcut that process.  Ideally, one should only need to pass
+#' it a species name and get out a nice big table of annotation data.
+#'
+#' @param species  String containing a unique portion of the desired species.
+#' @param webservice Which eupath webservice is desired?
+#' @param version Gather data from a specific eupathdb version?
+#' @param wanted_fields If not provided, this will gather all columns starting
+#'   with 'annot'.
+#' @return Big huge data frame of annotation data.
+#' @export
+load_eupath_annotations <- function(species="Leishmania major", webservice="tritrypdb",
+                                    version=NULL, wanted_fields=NULL) {
+  metadata <- download_eupath_metadata(webservice=webservice)
+  pkg_names <- get_eupath_pkgnames(species=species, metadata=metadata, version=version)
+  pkg_installedp <- pkg_names[["orgdb_installed"]]
+  if (isFALSE(pkg_installedp)) {
+    stop("The required package is not installed.")
+  }
+  pkg <- as.character(pkg_names[["orgdb"]])
+
+  if (is.null(wanted_fields)) {
+    org_pkgstring <- glue("library({pkg}); pkg <- {pkg}")
+    eval(parse(text=org_pkgstring))
+    all_fields <- AnnotationDbi::keytypes(x=pkg)
+    annot_fields_idx <- grepl(pattern="^ANNOT", x=all_fields)
+    annot_fields <- all_fields[annot_fields_idx]
+    wanted_fields <- c("gid", annot_fields)
+  }
+  org <- load_orgdb_annotations(pkg, keytype="gid", fields=wanted_fields)[["genes"]]
+  colnames(org) <- gsub(pattern="^annot_", replacement="", x=colnames(org))
+  kept_columns <- !duplicated(colnames(org))
+  org <- org[, kept_columns]
+  return(org)
+}
+
 #' Create an organismDbi instance for an eupathdb organism.
 #'
 #' The primary goal of an organismdbi instance is to provide a series of links
