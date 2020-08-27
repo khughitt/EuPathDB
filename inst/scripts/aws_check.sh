cdb0b1a8643e01157ce04e5e9def97950eef5607
Ashton Trey Belew <abelew@gmail.com>
2020-08-26 20:05:01 -0400

    moved my pile-o-scripts

--- inst/scripts/aws_check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-aws --profile AnnotationContributor s3api list-objects --bucket annotationhub --page-size 8000 --query 'Contents[].{Key: Key}' | cut -f2 -d$'\t' | grep '^EuPathDB/GRanges/3.10' | cut -f4 -d'/' > GRangeS3List.txt
-aws --profile AnnotationContributor s3api list-objects --bucket annotationhub --page-size 8000 --query 'Contents[].{Key: Key}' | cut -f2 -d$'\t' | grep '^EuPathDB/OrgDb/3.10' | cut -f4 -d'/' > OrgDbS3List.txt
-aws --profile AnnotationContributor s3api list-objects --bucket annotationhub --page-size 8000 --query 'Contents[].{Key: Key}' | cut -f2 -d$'\t' | grep '^EuPathDB/TxDb/3.10' | cut -f4 -d'/' > TxDbS3List.txt
